700eaf686a9718b013e92a46a5acf1833f0d4ad6
namuun222 <namuna.boldbayar@gmail.com>
2026-07-15 16:14:26 +0200

    change

--- cnn_model/src/model_config.h
+++ cnn_model/src/model_config.h
@@ -4,66 +4,60 @@
 #include "arm_nnfunctions.h"
 #include "weights.h"
 
-// Model config
-#define NUM_CONV_LAYERS  4
-#define NUM_POOL_LAYERS  3
-#define NUM_FC_LAYERS    2
-#define NUM_CLASSES      10
-#define MAX_ACTIVATION_SIZE  (28*28*32)
-#define NUM_BUFFERS 9
-
-#define MRAM_BASE_ADDR   0x00080000 
-#define MRAM_CHECKPOINT  ((uint32_t *)(MRAM_BASE_ADDR + 0x0000))
-
-// Layer outputs stored sequentially in MRAM
-#define MRAM_BUF0   ((q7_t *)(MRAM_BASE_ADDR + 0x1000)) // 25088 bytes
-#define MRAM_BUF1   ((q7_t *)(MRAM_BASE_ADDR + 0x8000)) //  6272 bytes
-#define MRAM_BUF2   ((q7_t *)(MRAM_BASE_ADDR + 0xA000)) //  6272 bytes
-#define MRAM_BUF3   ((q7_t *)(MRAM_BASE_ADDR + 0xC000)) //  1568 bytes
-#define MRAM_BUF4   ((q7_t *)(MRAM_BASE_ADDR + 0xD000)) //  3136 bytes
-#define MRAM_BUF5   ((q7_t *)(MRAM_BASE_ADDR + 0xE000)) //  3136 bytes
-#define MRAM_BUF6   ((q7_t *)(MRAM_BASE_ADDR + 0xF000)) //  1024 bytes
-#define MRAM_BUF7   ((q7_t *)(MRAM_BASE_ADDR + 0xF800)) //   256 bytes
-#define MRAM_OUTPUT ((q7_t *)(MRAM_BASE_ADDR + 0xFC00)) //    10 bytes
-
-#define MAX_KERNEL_SIZE   (3)
-#define MAX_INPUT_CH      (64)
+//*****************************************************************************
+// Generic constants (hardware limits, not model specific)
+//*****************************************************************************
+#define MAX_ACTIVATION_SIZE  (28*28*64)   // safe upper bound for small CNNs
+#define MAX_KERNEL_SIZE      (3)
+#define MAX_INPUT_CH         (64)
 #define SCRATCH_Q15_SIZE \
     (2 * MAX_KERNEL_SIZE * MAX_KERNEL_SIZE * MAX_INPUT_CH)
 
+//*****************************************************************************
+// MRAM addresses (Stage 3 will make these automatic)
+//*****************************************************************************
+#define MRAM_BASE_ADDR   0x0006F000
+#define MRAM_CHECKPOINT  ((uint32_t *)(MRAM_BASE_ADDR + 0x0000))
+#define MRAM_BUFFERS_START  (MRAM_BASE_ADDR + 0x1000)
+
+//*****************************************************************************
 // Layer types
+//*****************************************************************************
 typedef enum {
     LAYER_CONV,
     LAYER_POOL,
-    LAYER_FC
+    LAYER_FC,
+    LAYER_RELU,      // in-place
+    LAYER_SOFTMAX,   // in-place
+    LAYER_FLATTEN    // no-op for HWC (future use)
 } LayerType_t;
 
-// Convolution layer config
+//*****************************************************************************
+// Layer configs
+//*****************************************************************************
 typedef struct {
     const q7_t *weights;
     const q7_t *bias;
-    uint16_t    in_dim;      // input width/height
-    uint16_t    in_ch;       // input channels
-    uint16_t    out_ch;      // output channels
-    uint8_t     kernel;      // kernel size
+    uint16_t    in_dim;
+    uint16_t    in_ch;
+    uint16_t    out_ch;
+    uint8_t     kernel;
     uint8_t     padding;
     uint8_t     stride;
     uint8_t     bias_shift;
     uint8_t     out_shift;
-    uint16_t    out_dim;     // output width/height
+    uint16_t    out_dim;
 } ConvConfig_t;
 
-// Pool layer config
 typedef struct {
-    uint16_t    in_dim;      // input width/height
-    uint16_t    ch;          // channels
+    uint16_t    in_dim;
+    uint16_t    ch;
     uint8_t     kernel;
     uint8_t     padding;
     uint8_t     stride;
-    uint16_t    out_dim;     // output width/height
+    uint16_t    out_dim;
 } PoolConfig_t;
 
-// FC layer config
 typedef struct {
     const q7_t *weights;
     const q7_t *bias;
@@ -73,9 +67,36 @@ typedef struct {
     uint8_t     out_shift;
 } FCConfig_t;
 
+//*****************************************************************************
+// Generic layer descriptor
+//*****************************************************************************
+typedef struct {
+    LayerType_t  type;               // CONV / POOL / FC / RELU / SOFTMAX
+    const void  *config;             // points to Conv/Pool/FC config (NULL for RELU etc)
+    uint8_t      input_buffer;       // 0 = act0, 1 = act1
+    uint8_t      output_buffer;      // 0 = act0, 1 = act1
+    uint8_t      checkpoint;  // MRAM address (NULL = no checkpoint)
+    uint32_t     output_size;        // output size in bytes
+} Layer_t;
 
-// Conv layers
-static const ConvConfig_t conv_layers[NUM_CONV_LAYERS] = {
+//*****************************************************************************
+// Model descriptor
+//*****************************************************************************
+typedef struct {
+    const char    *name;
+    const Layer_t *layers;
+    uint16_t       num_layers;
+    uint16_t       num_classes;
+    uint16_t       input_w;
+    uint16_t       input_h;
+    uint16_t       input_ch;
+    uint32_t       max_act_size;
+} ModelConfig_t;
+
+//*****************************************************************************
+// MNIST layer configs
+//*****************************************************************************
+static const ConvConfig_t conv_layers[] = {
     // Conv0: 28x28x1 -> 28x28x32
     { conv0_wt, conv0_bias, 28, 1,  32, 3, 1, 1, CONV0_BIAS_SHIFT, CONV0_OUT_SHIFT, 28 },
     // Conv1: 14x14x32 -> 14x14x32
@@ -84,40 +105,62 @@ static const ConvConfig_t conv_layers[NUM_CONV_LAYERS] = {
     { conv2_wt, conv2_bias,  7, 32, 64, 3, 1, 1, CONV2_BIAS_SHIFT, CONV2_OUT_SHIFT,  7 },
     // Conv3: 7x7x64 -> 7x7x64
     { conv3_wt, conv3_bias,  7, 64, 64, 3, 1, 1, CONV3_BIAS_SHIFT, CONV3_OUT_SHIFT,  7 },
-}; 
-
-
-// Pool layers
-static const PoolConfig_t pool_layers[NUM_POOL_LAYERS] = {
-    // Pool0: 28x28x32 -> 14x14x32
-    { 28, 32, 2, 0, 2, 14 },
-    // Pool1: 14x14x32 -> 7x7x32
-    { 14, 32, 2, 0, 2,  7 },
-    // Pool2: 7x7x64 -> 4x4x64
-    {  7, 64, 4, 0, 1,  4 },
 };
 
-
-// FC layers
-static const FCConfig_t fc_layers[NUM_FC_LAYERS] = {
-    // FC1: 1024 -> 256
-    { fc1_wt, fc1_bias, 1024, 256, FC1_BIAS_SHIFT, FC1_OUT_SHIFT },
-    // FCo: 256 -> 10
-    { fco_wt, fco_bias,  256,  10, FCO_BIAS_SHIFT, FCO_OUT_SHIFT },
+static const PoolConfig_t pool_layers[] = {
+    { 28, 32, 2, 0, 2, 14 },   // Pool0: 28x28x32 -> 14x14x32
+    { 14, 32, 2, 0, 2,  7 },   // Pool1: 14x14x32 -> 7x7x32
+    {  7, 64, 4, 0, 1,  4 },   // Pool2: 7x7x64  -> 4x4x64
 };
 
-//buffer sized for mram
-static const uint32_t buf_sizes[] = {
-    28*28*32,   // buf0: conv0 output  = 25088 bytes
-    14*14*32,   // buf1: pool0 output  =  6272 bytes
-    14*14*32,   // buf2: conv1 output  =  6272 bytes
-    7*7*32,     // buf3: pool1 output  =  1568 bytes
-    7*7*64,     // buf4: conv2 output  =  3136 bytes
-    7*7*64,     // buf5: conv3 output  =  3136 bytes
-    4*4*64,     // buf6: pool2 output  =  1024 bytes
-    256,        // buf7: fc1  output   =   256 bytes
-    10,         // buf8: fco  output   =    10 bytes
+static const FCConfig_t fc_layers[] = {
+    { fc1_wt, fc1_bias, 1024, 256, FC1_BIAS_SHIFT, FC1_OUT_SHIFT },  // FC1
+    { fco_wt, fco_bias,  256,  10, FCO_BIAS_SHIFT, FCO_OUT_SHIFT },  // FCo
 };
 
+//*****************************************************************************
+// MNIST network sequence (RELU and SOFTMAX explicit!)
+//*****************************************************************************
+static const Layer_t network[] = {
+    // Conv0: input -> act0, ReLU in-place
+    { LAYER_CONV,    &conv_layers[0], 0, 0, 1, 28*28*32 },
+    { LAYER_RELU,    NULL,            0, 0, 0, 28*28*32 },
+    // Pool0: act0 -> act1
+    { LAYER_POOL,    &pool_layers[0], 0, 1, 1, 14*14*32 },
+    // Conv1: act1 -> act0, ReLU
+    { LAYER_CONV,    &conv_layers[1], 1, 0, 1, 14*14*32 },
+    { LAYER_RELU,    NULL,            0, 0, 0, 14*14*32 },
+    // Pool1: act0 -> act1
+    { LAYER_POOL,    &pool_layers[1], 0, 1, 1,  7*7*32  },
+    // Conv2: act1 -> act0, ReLU
+    { LAYER_CONV,    &conv_layers[2], 1, 0, 1,  7*7*64  },
+    { LAYER_RELU,    NULL,            0, 0, 0,  7*7*64  },
+    // Conv3: act0 -> act1, ReLU
+    { LAYER_CONV,    &conv_layers[3], 0, 1, 1,  7*7*64  },
+    { LAYER_RELU,    NULL,            1, 1, 0,  7*7*64  },
+    // Pool2: act1 -> act0
+    { LAYER_POOL,    &pool_layers[2], 1, 0, 1,  4*4*64  },
+    // FC1: act0 -> act1, ReLU
+    { LAYER_FC,      &fc_layers[0],   0, 1, 1,  256     },
+    { LAYER_RELU,    NULL,            1, 1, 0,  256     },
+    // FCo: act1 -> act0
+    { LAYER_FC,      &fc_layers[1],   1, 0, 1,  10      },
+    // Softmax in-place (final)
+    { LAYER_SOFTMAX, NULL,            0, 0, 0,  10      },
+};
+
+//*****************************************************************************
+// MNIST model instance
+//*****************************************************************************
+static const ModelConfig_t mnist_model = {
+    .name         = "MNIST_CNN",
+    .layers       = network,
+    .num_layers   = sizeof(network) / sizeof(network[0]),
+    .num_classes  = 10,
+    .input_w      = 28,
+    .input_h      = 28,
+    .input_ch     = 1,
+    .max_act_size = 28*28*32,
+};
 
 #endif // MODEL_CONFIG_H
